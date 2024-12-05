a6293c88cd88f5974a5e6c5674921063fc161ec9
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-05 07:14:27 -0500

    host fail

--- db/connection.go
+++ db/connection.go
@@ -6,7 +6,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-var DSN = "host=host.docker.internal user=diegoalfonsoc18 password=Shadow1827$$ dbname=gorm port=5432 sslmode=disable"
+var DSN = "host=localhost user=diegoalfonsoc18 password=Shadow1827$$ dbname=gorm port=5432 sslmode=disable"
 
 
 var DB *gorm.DB
@@ -16,7 +16,8 @@ func DBConnection () {
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
-		log.Fatal(error)
+		log.Fatalf("Failed to connect to database: %v", error)
+		
 	} else {
 		log.Println("DB connected")
 	}
